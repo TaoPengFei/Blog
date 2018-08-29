06a870d01d7d52b8e9aa654936f21bed06044b8b
TaoPengFei <taopengfei1223@foxmail.com>
2018-08-29 10:37:45 +0800

    解决‘(node:34612) DeprecationWarning: current URL string parser is deprecated, an'的问题...

--- src/init/mongodb.js
+++ src/init/mongodb.js
@@ -3,7 +3,14 @@
 import mongoose from 'mongoose';
 
 module.exports = function (done) {
-    const conn = mongoose.createConnection($.config.get('db.mongodb'));
+    const conn = mongoose.createConnection($.config.get('db.mongodb'),{useNewUrlParser:true},);
+    /*const conn = mongoose.connect($.config.get('db.mongodb'),{useNewUrlParser:true}, function (err) {
+        if (err){
+            console.log('Connection Error:' + err);
+        } else {
+            console.log('Connection success!');
+        }
+    });*/
 
     $.mongodb = conn;
 
